80c28acca5b3479663c5389c7a5c1e84f9c525a8
Mahammad Arshil Vahora <145360300+MahammadArshil@users.noreply.github.com>
2024-08-28 13:01:23 +0530

    First

---
+++ CRUD_with_EmployeeDB/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Program_41.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Program_41.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CRUD_with_EmployeeDB/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Program_41.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Employee Name")]
+        public required string Name { get; set; }
+        public required string Designation { get; set; }
+        [DataType(DataType.MultilineText)]
+        public required string Address { get; set; }
+        public DateTime? RecordCreatedOn { get; set; }
+    }
+}

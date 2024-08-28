09b8ddea9fcdac2b7d50e3bb7c3c519cc14db01f
Mahammad Arshil Vahora <145360300+MahammadArshil@users.noreply.github.com>
2024-08-28 14:10:20 +0530

    7

---
+++ View_Data_in_Core/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Program_40.Models;
+using System.Diagnostics;
+
+namespace Program_40.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Prop1"] = "Arshil Vahora";
+            ViewData["Prop2"] = new ErrorViewModel() { Id = 1, Name = "Arshil" };
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ View_Data_in_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Program_40.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public int Id {get; set; }
+        public string Name {get; set; }
+    }
+}

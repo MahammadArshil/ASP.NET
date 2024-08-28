f5312026509a1b594affa1b13601a2c642124c87
Mahammad Arshil Vahora <145360300+MahammadArshil@users.noreply.github.com>
2024-08-28 13:09:36 +0530

    3 and 4

---
+++ For_&_Foreach_Loop_inCOre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Program_37.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

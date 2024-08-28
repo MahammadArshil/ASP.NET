805d5d701253cd6282b3b30a5e2abcf5e8a0af5c
Mahammad Arshil Vahora <145360300+MahammadArshil@users.noreply.github.com>
2024-08-28 14:16:50 +0530

    10

---
+++ Working_of_View_in_Core/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Program_32.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}

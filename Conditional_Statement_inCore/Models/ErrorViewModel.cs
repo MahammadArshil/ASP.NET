45c03e0656a76017bd18265cac5f154b0584f3a0
Mahammad Arshil Vahora <145360300+MahammadArshil@users.noreply.github.com>
2024-08-28 13:03:24 +0530

    Second

---
+++ Conditional_Statement_inCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Program_36.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

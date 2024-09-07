65a2450d23647d1b5b30f1b38fc78263bd54a42b
Mahammad Arshil Vahora <145360300+MahammadArshil@users.noreply.github.com>
2024-09-07 10:10:54 +0530

    MVC in .NET uploading

---
+++ MVC_Controller_Demo(.NET)/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Controller_Demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public string EmployeeProfile(int id)
+        {
+            string profile = string.Empty;
+            if(id==1)
+            {
+                profile = "Employee 1 Profile";
+            }
+            else if (id == 2)
+            {
+                profile = "Employee 2 Profile";
+            }
+            else
+            {
+                profile = "NO Record Found.";
+            }
+            return profile;
+        }
+
+        public string Address(int id , int? code=null)
+        {
+            return "id =" + id + "dept=" + code;
+        }
+    }
+}
---
+++ MVC_Controller_Demo(.NET)/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Controller_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from College";
+        }
+        public string Name()
+        {
+            return "My name is Arshil";
+        }
+    }
+}
